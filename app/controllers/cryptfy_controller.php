25a8c3ff6ffba827c791328f2ce4654622316081
Djalma Araújo de Andrade <djalma.araujo@gmail.com>
2012-05-17 19:38:28 -0300

    Returning real string in results

--- app/controllers/cryptfy_controller.php
+++ app/controllers/cryptfy_controller.php
@@ -4,11 +4,14 @@ class CryptfyController extends AppController {
 	public function index() {
 		if ($this->data) {
 			if ($this->data['string']) {
-				$results = $this->Cryptfy->getAll($this->data['string']);
+				$string 	= $this->data['string'];
+				$results 	= $this->Cryptfy->getAll($string);
+
 			} else {
 				$results = array(
-					'error' => 'invalid_string',
-					'results' => false
+					'error' 	=> 'invalid_string',
+					'string' 	=> $string,
+					'results' 	=> false
 				);
 			}
 
@@ -18,15 +21,14 @@ class CryptfyController extends AppController {
 
 
 	public function output($results) {
-
 		switch ($this->param('extension')) {
 			case 'json':
 				$this->autoRender = false;
 				$this->JSONOutput(array('results' => $results));
 				break;
-
 			default:
 				$this->set('results', $results);
+				$this->set('string', $this->data['string']);
 		}
 	}
 }
